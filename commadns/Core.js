70964dff96faa5c13917b27e28412416109f75cb
Windows - osu! - <72699183+windowsModern@users.noreply.github.com>
2021-08-06 18:49:58 +0000

    finished help command enough shit for toady

--- commadns/Core.js
+++ commadns/Core.js
@@ -5,4 +5,13 @@ module.exports = [{
 Bot ping : $pingms
 DB ping  : $dbpingms\`\`\`]
   $footer[$getVar[botver]] $color[$getVar[color]]`
+},
+{
+  name: "help",
+  code: `$TITLE[Myaru] $DESCRIPTION[Hello, I am the bot that made Windows regret living by just coding me. [Join this server](https://discord.gg/TPw3gcm9XU)
+  \`\`\`yaml
+These comma:nds might be helpful.
+m.help     : Come on, you know.
+m.commands : See all of $commandsCount commands.\`\`\`]
+  $footer[$getVar[botver]] $color[$getVar[color]]`
 }]
